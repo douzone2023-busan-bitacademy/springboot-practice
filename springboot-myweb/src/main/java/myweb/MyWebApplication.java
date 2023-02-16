b06e62f08f33b8866d161dd24eba74a8e754a473
douzone2023-busan-bitacademy <douzone2023.busan.bitacademy@gmail.com>
2023-02-16 10:25:07 +0900

    springboot-myweb #finish

--- springboot-myweb/src/main/java/myweb/MyWebApplication.java
+++ springboot-myweb/src/main/java/myweb/MyWebApplication.java
@@ -13,7 +13,7 @@ public class MyWebApplication {
 	public class HelloController {
 		@RequestMapping("/hello")
 		public String hello() {
-			return "/WEB-INF/views/hello.jsp";
+			return "hello";
 		}
 	}
 	
